d47e2cd3b16e588f4822a7b1ac5424f6f359ea01
Thiago Miziara <thiago.miziara@luizalabs.com>
2025-07-30 15:40:06 -0300

    feat: implementing password handler

---
+++ src/drivers/password_handler_test.py
@@ -0,0 +1,11 @@
+from .password_handler import PasswordHandler
+
+
+def test_encrypt():
+    password = "123python"
+    password_handler = PasswordHandler()
+
+    hashed_password = password_handler.encrypt_password(password)
+    password_checked = password_handler.check_password(password, hashed_password)
+
+    assert password_checked
